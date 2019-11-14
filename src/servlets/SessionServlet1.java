4deee8ed630e49c57acecf0234fe4d00299bc67e
Vitali Orlioglo <vitalik.orlioglo@gmail.com>
2019-11-14 16:37:37 +0100

    nichts besonderes

--- src/servlets/SessionServlet1.java
+++ src/servlets/SessionServlet1.java
@@ -2,26 +2,34 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-@WebServlet(name = "paramServlet", urlPatterns = "/param")
-public class ParamServlet extends HttpServlet{
+@WebServlet(name = "sessionServlet1", urlPatterns = "/session1")
+public class SessionServlet1 extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //		super.doGet(req, resp);
+		HttpSession session = req.getSession(true); // erzeugt eine Session, when paramter true ist
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
 		out.println("<html><head><title>Title</title></head> <body>");
 		
-		out.print("<h1>HAllo ParamServlet . . . </h1>");
+		out.print("<h1>HAllo SessionServlet1 . . . </h1>");
 		
-		out.print(req.getParameter("vorname"));
+		String vorname = req.getParameter("vorname");
+		out.print(vorname);
 		
+		out.print("<a href = '/app1/session2'> weiter </a> ");
 		out.println("</body></html>");
+		
+		session.setAttribute("vorname", vorname);
+		session.setAttribute("date", LocalDate.now());
 	}
 }
--- src/servlets/SessionServlet2.java
+++ src/servlets/SessionServlet2.java
@@ -2,22 +2,33 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class HelloServlet extends HttpServlet{
+@WebServlet(name = "sessionServlet2", urlPatterns = "/session2")
+public class SessionServlet2 extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //		super.doGet(req, resp);
+		HttpSession session = req.getSession(); // session holen
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
 		out.println("<html><head><title>Title</title></head> <body>");
 		
-		out.print("<h1>HAllo Servlet . . . </h1>");
+		out.print("<h1>HAllo SessionServlet2 . . . </h1>");
 		
+		String vorname = String.valueOf(session.getAttribute("vorname"));
+		LocalDate date = (LocalDate) session.getAttribute("date");
+		
+		out.print(vorname + "" + date);
+		
+//		out.print("<a href = '/app1/session2'> weiter </a> ");
 		out.println("</body></html>");
 	}
 }
